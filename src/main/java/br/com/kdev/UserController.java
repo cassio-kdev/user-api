29f49a17a6946806c3e563c0993da79cf182176b
Cássio Alves Silva <cassio.alves09@gmail.com>
2021-06-04 16:34:23 -0300

    Correção de importe

--- src/main/java/br/com/kdev/UserController.java
+++ src/main/java/br/com/kdev/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.kdev.model.dto.UserDTO;
+import br.com.kdev.dto.UserDTO;
 
 @RestController
 public class UserController {
